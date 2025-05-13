c54c2540a7a0fb74bdca96358678ec163f6942a8
Aleksandr Smyk <a.smyk.1989@gmail.com>
2025-05-13 15:02:22 +0200

    updates

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -20,7 +20,7 @@ public abstract class BasePageViewModel<TItemViewModel> : BaseBindable
 
     private void OnTestCommand(object obj)
     {
-        Application.Current.MainPage.DisplayAlert("Test", "XamlC works well ", "OK");
+        Application.Current.MainPage.DisplayAlert("Test", "XamlC works well", "OK");
     }
 }
 
@@ -40,23 +40,9 @@ public class MainPageViewModel : BasePageViewModel<MainPageModel>
 
 public partial class MainPage : BaseContentPage<BasePageViewModel<BaseModel>>
 {
-    int count = 0;
-
     public MainPage()
     {
         BindingContext = new MainPageViewModel();
         InitializeComponent();
-    }
-
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
     }
 }
